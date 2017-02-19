1db365fa7d9c8ff5b38f8e7a1eef8d93ef8f0d06
johnjogrady <kings07park>
2017-02-19 12:19:06 +0000

    Separate customer controller, list view for customer controlller updated and working

--- src/controllers/CustomerController.php
+++ src/controllers/CustomerController.php
@@ -9,6 +9,7 @@
 namespace Itb\Controller;
 
 use Itb\Model\CustomerRepository;
+use Itb\Model\CustomerRepositoryView;
 use Itb\WebApplication;
 
 class CustomerController
@@ -23,15 +24,69 @@ class CustomerController
     public function listAction()
     {
         // get reference to our repository
-        $customerRepository = new CustomerRepository();
+        $customerRepository = new CustomerRepositoryView();
         $customers = $customerRepository->getAll();
 
         $argsArray = [
             'customers' => $customers
         ];
-        $templateName = 'customers';
+        $templateName = 'Customers\list';
               return $this->app['twig']->render($templateName . '.html.twig', $argsArray);
     }
 
+    public function updateAction()
+    {
+        // get reference to our repository
+        $customerRepository = new CustomerRepository();
+        $customers = $customerRepository->getAll();
+        //to do update to get one by id
+         $argsArray = [
+            'customer' => $customers
+        ];
 
+        $templateName = 'Customers\update';
+        return $this->app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
+
+    public function deleteAction()
+    {
+        // get reference to our repository
+        $customerRepository = new CustomerRepository();
+        $customers = $customerRepository->getAll();
+        //to do update to get one by id
+        $argsArray = [
+            'customer' => $customers
+        ];
+
+        $templateName = 'Customers\delete';
+        return $this->app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
+
+    public function createAction()
+    {
+        // get reference to our repository
+        $customerRepository = new CustomerRepository();
+        $customers = $customerRepository->getAll();
+        //to do update to get one by id
+        $argsArray = [
+            'customer' => $customers
+        ];
+
+        $templateName = 'Customers\create';
+        return $this->app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
+
+    public function showAction()
+    {
+        // get reference to our repository
+        $customerRepository = new CustomerRepository();
+        $customers = $customerRepository->getAll();
+        //to do update to get one by id
+        $argsArray = [
+            'customer' => $customers
+        ];
+
+        $templateName = 'Customers\show';
+        return $this->app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
 }
